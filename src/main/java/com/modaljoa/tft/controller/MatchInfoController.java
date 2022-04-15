f76bed8afd6ace3d539539f83c7b958f94b3c3fc
Kwon seong jun <tjdwns1865@naver.com>
2022-04-16 05:53:28 +0900

    feat: 매치 id로 매치 정보 얻기

---
+++ src/main/java/com/modaljoa/tft/controller/MatchInfoController.java
@@ -0,0 +1,20 @@
+package com.modaljoa.tft.controller;
+
+import com.modaljoa.tft.dto.MatchDto;
+import com.modaljoa.tft.service.MatchInfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MatchInfoController {
+
+    private final MatchInfoService matchInfoService;
+
+    @GetMapping("/tft/match/v1/matches/{matchId}")
+    public MatchDto findMatchInfo(@PathVariable String matchId) {
+        return matchInfoService.getMatchInfo(matchId);
+    }
+}
---
+++ src/main/java/com/modaljoa/tft/service/MatchInfoService.java
@@ -0,0 +1,29 @@
+package com.modaljoa.tft.service;
+
+import com.modaljoa.tft.dto.MatchDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import static com.modaljoa.tft.info.StringInfo.*;
+
+@Service
+@RequiredArgsConstructor
+public class MatchInfoService {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
+    public MatchDto getMatchInfo(String matchId) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.ACCEPT_CHARSET, "application/x-www-form-urlencoded; charset=UTF-8");
+        headers.set(HttpHeaders.ORIGIN, requestHeaderOrigin);
+
+        MatchDto matchInfo = restTemplate.getForObject(findMatchInfoByMatchId + matchId + "?api_key=" + apiKey, MatchDto.class);
+
+        return matchInfo;
+    }
+}
