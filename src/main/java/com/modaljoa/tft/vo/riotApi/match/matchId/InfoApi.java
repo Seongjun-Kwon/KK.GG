5e8010d0f89dfcf1cfe95eedf4412017663a4081
Kwon seong jun <tjdwns1865@naver.com>
2022-08-03 21:07:11 +0900

    refactor: riot api 로부터 오는 json 필드명과 객체 필드명 차이 해결

--- src/main/java/com/modaljoa/tft/vo/riotApi/match/matchId/InfoApi.java
+++ src/main/java/com/modaljoa/tft/vo/riotApi/match/matchId/InfoApi.java
@@ -1,16 +1,28 @@
 package com.modaljoa.tft.vo.riotApi.match.matchId;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class InfoApi {
+    @JsonProperty(value = "game_datetime")
     private long gameDatetime;
+
+    @JsonProperty(value = "game_length")
     private float gameLength;
+
+    @JsonProperty(value = "game_variation")
     private String gameVariation;
+
+    @JsonProperty(value = "game_version")
     private String gameVersion;
     private List<ParticipantApi> participants;
+
+    @JsonProperty(value = "queue_id")
     private int queueId;
+
+    @JsonProperty(value = "tft_set_number")
     private int tftSetNumber;
 }
--- src/main/java/com/modaljoa/tft/vo/riotApi/match/matchId/MetadataApi.java
+++ src/main/java/com/modaljoa/tft/vo/riotApi/match/matchId/MetadataApi.java
@@ -1,12 +1,16 @@
 package com.modaljoa.tft.vo.riotApi.match.matchId;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class MetadataApi {
+    @JsonProperty(value = "data_version")
     private String dataVersion;
+
+    @JsonProperty(value="match_id")
     private String matchId;
     private List<String> participants;
 }
