01bcde1e1377089646484b3995901e2af6ee3eeb
Kwon seong jun <tjdwns1865@naver.com>
2022-09-01 03:13:10 +0900

    refactor: 랭킹 화면 관련 수정사항 - 랭킹 홈 화면 경로가 "/ranking" 이 아니라 "/ranking/" 이었던 문제를 해결. 쿼리 파라미터 형식으로 티어 정보를 받게 하여 해결하였음. 또한, 추후에 지역, 모드에 따라서도 랭킹을 나눌 것이므로 쿼리 파라미터 방식이 더 적절하다고 판단하였음.

--- src/main/java/com/modaljoa/tft/controller/RankingController.java
+++ src/main/java/com/modaljoa/tft/controller/RankingController.java
@@ -1,16 +1,15 @@
 package com.modaljoa.tft.controller;
 
 import com.modaljoa.tft.dto.RankingDTO;
-import com.modaljoa.tft.dto.SummonerLeagueDTO;
 import com.modaljoa.tft.service.RankingService;
 import com.modaljoa.tft.vo.domain.TierType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //@RestController
 @Controller
@@ -30,8 +29,8 @@ public class RankingController {
         return "rankingHome";
     }
 
-    @GetMapping("/{tier}")
-    public String getTopTierUsers(@PathVariable String tier, Model model) {
+    @GetMapping(params = "tier")
+    public String getTopTierUsers(@RequestParam String tier, Model model) {
         RankingDTO rankingDTO = rankingService.getTopTierUsers(tier);
         model.addAttribute(rankingDTO);
 
